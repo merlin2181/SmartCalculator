35508d96f0f4a57316dbd82bb9e9103a8afd7b99
merlin2181 <merlin2181@gmail.com>
2020-08-22 14:09:15 -0700

    Stage 2 of 7 of the Smart Calculator project from JetBrains Academy
    - Added ability to recognize if numbers are entered, text is entered or nothing is entered
    - Program now prints out a single number if a single number is entered.
    - If nothing is entered, it asks the user for input again.
    - User can exit the program by typing '/exit'. If any other text is entered, the program prints an error message and asks for input again.

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,1 +1,40 @@
-print(sum(int(i) for i in input().split()))
+import sys
+
+class Calculator:
+
+    def __init__(self):
+        self.numbers = None
+        self.flag = None
+
+    def main(self):
+        """
+        Main method that runs the calculator. It currently prints the sum of user inputted numbers.
+        """
+        while True:
+            self.flag = True
+            self.numbers = input('Please enter two numbers or "/exit" to quit:\n').split()
+            for input_ in self.numbers:
+                if input_ == '/exit':
+                    print('Bye!')
+                    sys.exit()
+                # Makes sure the user enters numbers (1, 823, 7732, etc.) not text
+                try:
+                    int(input_)
+                except ValueError:  # If text is entered, returns error message and starts loop over.
+                    print('Please enter numbers only.')
+                    print('')
+                    self.flag = False
+                    break
+            if self.flag:
+                if len(self.numbers) == 0:
+                    continue
+                if len(self.numbers) == 1:
+                    print(self.numbers[0])
+                else:
+                    print(sum(int(i) for i in self.numbers))
+                    print('')
+
+
+if __name__ == '__main__':
+    calc = Calculator()
+    calc.main()
