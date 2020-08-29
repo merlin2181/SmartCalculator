bd219253bbb01dc00ccd4be8f9246e08bfd78b58
merlin2181 <merlin2181@gmail.com>
2020-08-28 22:42:59 -0700

    Stage 6 of 7 of Smart Calculator project from JetBrains Academy
    - Added the ability to use variables in calculations.
    - Updated the help message
    - Refactored code to catch more errors and added comments

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -5,14 +5,18 @@ class Calculator:
 
     def __init__(self):
         self.numbers = None
-        self.help = 'This program calculates the total of numbers you enter\n' \
+        self.help = 'This program calculates the total of the numbers you enter\n' \
                     'Enter numbers with the following syntax:\n' \
                     '-9 + 10 - 34 - -44\n' \
                     'a double negative "--" will turn into "+"\n' \
                     'example: -4 -- 4 -- -5 will be converted into: -4 + 4 + -5\n' \
                     'if you enter your equation without spaces, the program will print "Invalid expression" error.\n' \
+                    'This program also handles using variables\n' \
+                    'To use this feature, simply enter your variable and its assignment\n' \
+                    'example: a = 8, m=4, bb = a\n' \
                     '"/exit" will quit the program\n' \
                     '"/help" brings up this text.\n'
+        self.variables = {}
 
     def main(self):
         """
@@ -32,43 +36,78 @@ class Calculator:
         """
         if len(self.numbers) == 0:
             return False
-        elif self.numbers[0] == '/exit':
-            print('Bye!')
-            sys.exit()
-        elif self.numbers[0] == '/help':
-            print(self.help)
-            return False
-        elif self.numbers[0].startswith("/"):
-            print('Unknown command')
-            return False
         elif len(self.numbers) == 1:
-            try:
-                print(int(self.numbers[0]))
-            except (ValueError, SyntaxError):
-                print('Invalid expression')
-            finally:
-                return False
+            self.numbers = self.numbers[0]
+            self.check_input_length_one()
+        elif self.numbers.count('=') > 1:
+            print('Invalid assignment')
+            return False
+        elif 1 < len(self.numbers) <= 3:
+            self.check_variable()
+            return False
         return True
 
-    def check_parsed_input(self):
+    def check_input_length_one(self):
         """
-        Method that checks that the user entered equation is syntactically correct
+        Method for checking when the user only enters one item on the command line
         """
-        if '-' not in self.numbers and '+' not in self.numbers:
-            print('Invalid expression')
-            return False
-        if len(self.numbers) % 2 == 1:
-            for i, item in enumerate(self.numbers):
-                if i % 2 == 0:
-                    if type(item) == int:
-                        continue
-                    else:
-                        print('Invalid expression')
-                        return False
+        if self.numbers == '/exit':
+            print('Bye!')
+            sys.exit()
+        elif self.numbers == '/help':
+            print(self.help)
+        elif self.numbers.startswith("/"):
+            print('Unknown command')
+        elif '=' in self.numbers:
+            self.check_variable()
         else:
-            print('Invalid expression')
-            return False
-        return True
+            try:
+                print(self.variables[self.numbers])
+            except KeyError:
+                try:
+                    print(int(self.numbers))
+                except (ValueError, SyntaxError):
+                    if not self.numbers.isalpha():
+                        print('Invalid identifier')
+                    else:
+                        print('Unknown Variable')
+        self.main()
+
+    def check_variable(self):
+        """
+        Method that checks whether a legal variable statement was entered or not.
+        """
+        if "=" not in self.numbers and '+' in self.numbers or '-' in self.numbers:
+            self.calculate()
+        elif len(self.numbers) == 3 and self.numbers[1] == '=':
+            if not self.numbers[0].isalpha():
+                print('Invalid identifier')
+                self.main()
+            elif not self.numbers[2].isdigit() and not self.numbers[2].isalpha():
+                print('Invalid assignment')
+                self.main()
+            else:
+                self.dictionary_variable_check(self.numbers[0], self.numbers[2])
+        else:
+            for i, item in enumerate(self.numbers):
+                if i == 0 and len(item) > 1 and item.index('=') != 0:
+                    self.dictionary_variable_check(item[:item.index('=')], self.numbers[1])
+                elif i == 1 and len(item) > 1 and item.index('=') == 0:
+                    self.dictionary_variable_check(self.numbers[0], item[1:])
+
+    def dictionary_variable_check(self, key, value):
+        """
+        Method that checks the dictionary for any corresponding variable values.
+        """
+        try:
+            self.variables[key] = int(value)
+        except ValueError:
+            try:
+                if self.variables[value]:
+                    self.variables[key] = self.variables[value]
+            except KeyError:
+                print('Unknown variable')
+                self.main()
 
     def parse_input(self):
         """
@@ -89,21 +128,46 @@ class Calculator:
                     else:
                         self.numbers[i] = '-'
 
+    def check_parsed_input(self):
+        """
+        Method that checks that the user entered equation is syntactically correct
+        """
+        if '-' not in self.numbers and '+' not in self.numbers:
+            print('Invalid expression')
+            return False
+        if len(self.numbers) % 2 == 1:
+            for i, item in enumerate(self.numbers):
+                if i % 2 == 0:
+                    try:
+                        if type(item) == int or self.variables[item]:
+                            continue
+                    except ValueError:
+                        print('Invalid expression')
+                        return False
+                    except KeyError:
+                        print(f'Unknown variable "{item}" in equation')
+                        return False
+        else:
+            print('Invalid expression')
+            return False
+        return True
+
     def calculate(self):
         """
         Method that calculates the parsed equation and prints the answer
         """
         while len(self.numbers) > 1:
             evaluate = [self.numbers[0], self.numbers[1], self.numbers[2]]
+            for i, item in enumerate(evaluate):
+                if item in self.variables:
+                    evaluate[i] = self.variables[item]
             del self.numbers[:3]
             if '-' in evaluate:
                 result = evaluate[0] - evaluate[2]
-                self.numbers.insert(0, result)
             elif '+' in evaluate:
                 result = evaluate[0] + evaluate[2]
-                self.numbers.insert(0, result)
-        self.numbers = self.numbers[0]
-        print(self.numbers)
+            self.numbers.insert(0, result)
+        print(self.numbers[0])
 
 
 if __name__ == '__main__':
