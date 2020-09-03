4b2dd43b1cc2b65b0a944a52ad60b0bb7704d17b
merlin2181 <merlin2181@gmail.com>
2020-09-02 20:47:42 -0700

    Final stage of the Smart Calculator project from JetBrains Academy
    - a user entered expression is now converted to postfix notation and calculated from that.
    - refactored some code and added comments.
    - added more tests to catch user input errors

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,3 +1,4 @@
+from collections import deque
 import sys
 
 
@@ -5,29 +6,35 @@ class Calculator:
 
     def __init__(self):
         self.numbers = None
-        self.help = 'This program calculates the total of the numbers you enter\n' \
+        self.help = 'This program calculates the total of the expression you enter\n' \
+                    'You are allowed to use "(", ")", "^", "*", "/", "+", "-" operators\n' \
                     'Enter numbers with the following syntax:\n' \
-                    '-9 + 10 - 34 - -44\n' \
-                    'a double negative "--" will turn into "+"\n' \
-                    'example: -4 -- 4 -- -5 will be converted into: -4 + 4 + -5\n' \
-                    'if you enter your equation without spaces, the program will print "Invalid expression" error.\n' \
+                    '\t-9 + 10 - 34 - -44\n' \
+                    '\ta double negative "--" will turn into "+"\n' \
+                    '\texample: -4 -- 4 -- -5 will be converted into: -4 + 4 + -5\n' \
+                    'You can enter your equation with or without spaces.\n' \
+                    'Entering multiple "^", "*" or "/" will result in an "Invalid expression" error\n' \
+                    '\tas well as a lack of an opening "(" or closing ")" parenthesis\n' \
                     'This program also handles using variables\n' \
                     'To use this feature, simply enter your variable and its assignment\n' \
-                    'example: a = 8, m=4, bb = a\n' \
+                    '\texample: a = 8, m=4, bb = a\n' \
                     '"/exit" will quit the program\n' \
                     '"/help" brings up this text.\n'
         self.variables = {}
+        self.operators = ['^', '*', '/', '+', '-']
+        self.stack = deque()
+        self.postfix = deque()
+        self.holding = []
+        self.parsing = []
 
     def main(self):
         """
-        Main method that runs the calculator. It currently prints the total of user inputted numbers.
+        Main method that runs the calculator. It currently prints the total of user inputted expression.
         """
         while True:
-            self.numbers = input().split()
+            self.numbers = ''.join(input().split())
             if self.check_input():
-                self.parse_input()
-                if self.check_parsed_input():
-                    self.calculate()
+                self.calculate()
 
     def check_input(self):
         """
@@ -36,18 +43,12 @@ class Calculator:
         """
         if len(self.numbers) == 0:
             return False
-        elif len(self.numbers) == 1:
-            self.numbers = self.numbers[0]
-            self.check_input_length_one()
-        elif self.numbers.count('=') > 1:
-            print('Invalid assignment')
-            return False
-        elif 1 < len(self.numbers) <= 3:
-            self.check_variable()
-            return False
-        return True
+        elif "=" in self.numbers:
+            return self.assign_variables()
+        else:
+            return self.parse_input()
 
-    def check_input_length_one(self):
+    def parse_input(self):
         """
         Method for checking when the user only enters one item on the command line
         """
@@ -56,118 +57,224 @@ class Calculator:
             sys.exit()
         elif self.numbers == '/help':
             print(self.help)
+            return False
         elif self.numbers.startswith("/"):
             print('Unknown command')
-        elif '=' in self.numbers:
-            self.check_variable()
-        else:
+            return False
+        elif self.numbers.isalpha():
             try:
                 print(self.variables[self.numbers])
+                return False
             except KeyError:
-                try:
-                    print(int(self.numbers))
-                except (ValueError, SyntaxError):
-                    if not self.numbers.isalpha():
-                        print('Invalid identifier')
-                    else:
-                        print('Unknown Variable')
-        self.main()
+                print('Unknown variable')
+                return False
+        else:
+            try:
+                print(int(self.numbers))
+                return False
+            except ValueError:
+                if self.numbers.isalnum():
+                    print('Invalid identifier')
+                    return False
+                else:
+                    self.parse_infix()
+                    return True
 
-    def check_variable(self):
+    def assign_variables(self):
         """
         Method that checks whether a legal variable statement was entered or not.
         """
-        if "=" not in self.numbers and '+' in self.numbers or '-' in self.numbers:
-            self.calculate()
-        elif len(self.numbers) == 3 and self.numbers[1] == '=':
-            if not self.numbers[0].isalpha():
+        if self.numbers.count('=') > 1:
+            print('Invalid assignment')
+            return False
+        index = self.numbers.index('=')
+        if self.numbers[:index].isalpha():
+            if self.numbers[index + 1:].isdigit():
+                self.variables[self.numbers[:index]] = int(self.numbers[index + 1:])
+            elif self.numbers[index + 1:].isalpha():
+                try:
+                    self.variables[self.numbers[:index]] = self.variables[self.numbers[index + 1:]]
+                except KeyError:
+                    print('Invalid assignment')
+            elif not self.numbers[:index].isalpha():
                 print('Invalid identifier')
-                self.main()
-            elif not self.numbers[2].isdigit() and not self.numbers[2].isalpha():
+            elif not self.numbers[index + 1:].isalpha() or not self.numbers[index + 1:].isdigit():
                 print('Invalid assignment')
-                self.main()
-            else:
-                self.dictionary_variable_check(self.numbers[0], self.numbers[2])
         else:
-            for i, item in enumerate(self.numbers):
-                if i == 0 and len(item) > 1 and item.index('=') != 0:
-                    self.dictionary_variable_check(item[:item.index('=')], self.numbers[1])
-                elif i == 1 and len(item) > 1 and item.index('=') == 0:
-                    self.dictionary_variable_check(self.numbers[0], item[1:])
+            print('Invalid identifier')
+        return False
 
-    def dictionary_variable_check(self, key, value):
+    def parse_infix(self):
         """
-        Method that checks the dictionary for any corresponding variable values.
+        Method that parses the input from the user and separates it into operands
+        and operators.  It then goes to the remove_duplicates() method to remove
+        any duplicate '-' or '+' and check for multiple '^', '*' or '/'
         """
-        try:
-            self.variables[key] = int(value)
-        except ValueError:
-            try:
-                if self.variables[value]:
-                    self.variables[key] = self.variables[value]
-            except KeyError:
-                print('Unknown variable')
+        if self.numbers.count('(') == self.numbers.count(')'):
+            i = 0
+            while i < len(self.numbers):
+                if self.numbers[i].isdigit():
+                    while i < len(self.numbers) and self.numbers[i].isdigit():
+                        self.holding.append(self.numbers[i])
+                        i += 1
+                    self.parsing.append(''.join(self.holding))
+                    self.holding = []
+                elif self.numbers[i].isalpha():
+                    while i < len(self.numbers) and self.numbers[i].isalpha():
+                        self.holding.append(self.numbers[i])
+                        i += 1
+                    self.parsing.append(''.join(self.holding))
+                    self.holding = []
+                elif self.numbers[i] == '-':
+                    if self.numbers[i + 1].isdigit() and i == 0 or self.operator(self.numbers[i - 1]) or \
+                            self.numbers[i - 1] == '(':
+                        self.holding.append(self.numbers[i])
+                        i += 1
+                        while i < len(self.numbers) and self.numbers[i].isdigit():
+                            self.holding.append(self.numbers[i])
+                            i += 1
+                    else:
+                        self.holding.append(self.numbers[i])
+                        i += 1
+                    self.parsing.append(''.join(self.holding))
+                    self.holding = []
+                else:
+                    self.parsing.append(self.numbers[i])
+                    i += 1
+            self.parsing.append(')')
+            self.numbers = self.parsing
+            self.parsing = []
+            self.remove_duplicates()
+            if self.numbers is not None:
+                for i in range(len(self.numbers)):
+                    try:
+                        self.numbers[i] = int(self.numbers[i])
+                    except ValueError:
+                        continue
+                self.convert_to_postfix()
+            else:
+                print('Invalid expression')
                 self.main()
+        else:
+            print('Invalid expression')
+            self.main()
 
-    def parse_input(self):
+    def remove_duplicates(self):
         """
-        Method that parses the user's input into a functional equation
+        Method that parses the user's input, removes duplicate '-' or '+'
+        or returns an error message if multiple '*' or '/'
         i.e. 9 +++ 10 -- 8 turns into 9 + 10 + 8
              3 --- 5 turns into 3 - 5
              14        -   12 turns into 14 - 12
         """
-        for i, item in enumerate(self.numbers):
-            try:
-                self.numbers[i] = int(item)
-            except ValueError:
-                if len(self.numbers[i]) > 1:
-                    if '+' in self.numbers[i]:
-                        self.numbers[i] = '+'
-                    elif len(self.numbers[i]) % 2 == 0:
-                        self.numbers[i] = '+'
-                    else:
-                        self.numbers[i] = '-'
+        i = 0
+        end = len(self.numbers)
+        while i < end:
+            if self.numbers[i] == '-' and self.numbers[i + 1] == '-':
+                self.holding.append(self.numbers.pop(i))
+                first_index = i
+                while self.numbers[i] == '-':
+                    self.holding.append(self.numbers.pop(i))
+                if len(self.holding) % 2 == 0:
+                    self.holding = '+'
+                else:
+                    self.holding = '-'
+                self.numbers.insert(first_index, self.holding)
+                self.holding = []
+                end = len(self.numbers)
+                i += 1
+            elif self.numbers[i] == '+' and self.numbers[i + 1] == '+':
+                self.holding.append(self.numbers.pop(i))
+                index = i
+                while self.numbers[i] == "+":
+                    self.numbers.pop(i)
+                self.numbers.insert(index, self.holding[0])
+                self.holding = []
+                end = len(self.numbers)
+                i += 1
+            elif self.numbers[i] == '*' and self.numbers[i + 1] == '*' or \
+                    self.numbers[i] == '/' and self.numbers[i + 1] == '/' or \
+                    self.numbers[i] == '^' and self.numbers[i + 1] == '^':
+                self.numbers = None
+                break
+            else:
+                i += 1
 
-    def check_parsed_input(self):
+    def convert_to_postfix(self):
         """
-        Method that checks that the user entered equation is syntactically correct
+        Method that converts the parsed infix expression into a postfix expression
         """
-        if '-' not in self.numbers and '+' not in self.numbers:
-            print('Invalid expression')
-            return False
-        if len(self.numbers) % 2 == 1:
-            for i, item in enumerate(self.numbers):
-                if i % 2 == 0:
-                    try:
-                        if type(item) == int or self.variables[item]:
-                            continue
-                    except ValueError:
-                        print('Invalid expression')
-                        return False
-                    except KeyError:
-                        print(f'Unknown variable "{item}" in equation')
-                        return False
-        else:
-            print('Invalid expression')
-            return False
-        return True
+        self.stack.append('(')
+        for item in self.numbers:
+            if item == '(':
+                self.stack.append(item)
+            elif type(item) == int or item.isalpha():
+                self.postfix.append(item)
+            elif self.operator(item):
+                x = self.stack.pop()
+                while self.operator(x) and self.precedence(x) >= self.precedence(item):
+                    self.postfix.append(x)
+                    x = self.stack.pop()
+                self.stack.append(x)
+                self.stack.append(item)
+            elif item == ')':
+                x = self.stack.pop()
+                while x != '(':
+                    self.postfix.append(x)
+                    x = self.stack.pop()
+            else:
+                print('\nInvalid infix Expression.\n')
+                break
 
     def calculate(self):
         """
-        Method that calculates the parsed equation and prints the answer
+        Method that calculates the parsed postfix equation and prints out the answer
+        to the screen
         """
-        while len(self.numbers) > 1:
-            evaluate = [self.numbers[0], self.numbers[1], self.numbers[2]]
-            for i, item in enumerate(evaluate):
-                if item in self.variables:
-                    evaluate[i] = self.variables[item]
-            del self.numbers[:3]
-            if '-' in evaluate:
-                result = evaluate[0] - evaluate[2]
-            elif '+' in evaluate:
-                result = evaluate[0] + evaluate[2]
-            self.numbers.insert(0, result)
-        print(self.numbers[0])
+        for item in self.postfix:
+            if item in self.operators:
+                b = self.stack.pop()
+                a = self.stack.pop()
+                if item == '^':
+                    self.stack.append(a ** b)
+                elif item == '*':
+                    self.stack.append(a * b)
+                elif item == '/':
+                    self.stack.append(a / b)
+                elif item == '+':
+                    self.stack.append(a + b)
+                elif item == '-':
+                    self.stack.append(a - b)
+            elif type(item) == str:
+                try:
+                    self.stack.append(self.variables[item])
+                except KeyError:
+                    print('Unknown variable')
+                    self.stack = deque()
+                    self.postfix = deque()
+                    self.main()
+            else:
+                self.stack.append(item)
+        if type(self.stack[0]) == float:
+            if self.stack[0].is_integer():
+                self.stack[0] = int(self.stack[0])
+        print(self.stack[0])
+        self.stack = deque()
+        self.postfix = deque()
+
+    def operator(self, item):
+        return item in self.operators
+
+    @staticmethod
+    def precedence(item):
+        if item == '^':
+            return 5
+        if item in ['*', '/']:
+            return 3
+        if item in ['+', '-']:
+            return 1
+        else:
+            return 0
 
 
 if __name__ == '__main__':
